ec020fcd98bbd319a906dcecb35be4fe26b2f82a
Manan <manan132000@gmail.com>
2022-10-19 20:11:45 +0530

    commit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import JournalPage from "./JournalPage";
-import { topics } from "../utils/topics";
+// import { topics } from "../utils/topics";
 import "../css/Home.css";
 
 function Home(props) {
@@ -12,55 +12,55 @@ function Home(props) {
   useEffect(() => {
     // setTimeout(() => {}, 20000);
     props.setDisplayItems(["none", "none", "inline", "inline", "inline"]);
-    // axios.get(`http://localhost:5000/getAllJournals`).then((res) => {
-    //   setSpinnerVisible("hidden");
-    //   console.log(res.data);
-    //   setJournals(res.data);
-    // });
+    axios.get(`http://localhost:5000/getAllJournals`).then((res) => {
+      setSpinnerVisible("hidden");
+      console.log(res.data);
+      setJournals(res.data);
+    });
   }, []);
 
-  // if (spinnerVisible === "visible") {
-  //   return (
-  //     <span
-  //       style={{ visibility: spinnerVisible }}
-  //       className="spinner-border spinner-border-sm"
-  //       role="status"
-  //       aria-hidden="true"
-  //     />
-  //   );
-  // } else
-  return (
-    //   <Router>
-    <div className="home-div">
-      {/* <div class="container"> */}
-      <div className="row">
-        {topics.map((item, i) => {
-          return (
-            <div key={item} className="col col-md-3 card-div">
-              <Link to={`/${item}`} className="card-link">
-                <div className="card">
-                  <img
-                    className="card-img-top"
-                    src={`images/home-img-${i + 1}.jpg`}
-                    alt={item}
-                  />
-                  <div className="card-body">
-                    <p className="card-title">{item}</p>
-                    {/* <p class="card-text">
+  if (spinnerVisible === "visible") {
+    return (
+      <span
+        style={{ visibility: spinnerVisible }}
+        className="spinner-border spinner-border-sm"
+        role="status"
+        aria-hidden="true"
+      />
+    );
+  } else
+    return (
+      //   <Router>
+      <div className="home-div">
+        {/* <div class="container"> */}
+        <div className="row">
+          {journals.map((item) => {
+            return (
+              <div key={item._id} className="col col-md-3 card-div">
+                <Link to={`/${item._id}`} className="card-link">
+                  <div className="card">
+                    <img
+                      className="card-img-top"
+                      src={``}
+                      alt={item.journal_name}
+                    />
+                    <div className="card-body">
+                      <p className="card-title">{item.journal_name}</p>
+                      {/* <p class="card-text">
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p> */}
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </div>
-          );
-        })}
-        {/* </div> */}
+                </Link>
+              </div>
+            );
+          })}
+          {/* </div> */}
+        </div>
       </div>
-    </div>
-    //   </Router>
-  );
+      //   </Router>
+    );
 }
 
 export default Home;
