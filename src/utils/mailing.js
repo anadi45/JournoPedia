30f6ff6fa3e8c0280cc862c4909c09059f33f314
anadi45 <ranjeetbaraik15@gmail.com>
2022-11-01 21:19:40 +0530

    mailing system complete

---
+++ src/utils/mailing.js
@@ -0,0 +1,35 @@
+const nodemailer = require("nodemailer");
+const smtpTransport = require('nodemailer-smtp-transport');
+const MAIL_USER = process.env.MAIL_USER;
+const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
+
+const mail = (to,body) => {
+
+    const transport = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: MAIL_USER,
+            pass: MAIL_PASSWORD
+        }
+    }));
+    
+    const options = {
+        from: 'JournoPedia Team',
+        to: to,
+        subject: 'JournoPedia Team',
+        html: body
+    }
+    
+    transport.sendMail(options, (error, info) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('Sent' + info.response);
+        }
+    });
+}
+
+module.exports = {mail};
