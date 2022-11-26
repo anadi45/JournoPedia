dd3d10e7b29acda45fbfb9ba038b6a42efbb1d5b
Manan Agarwal <manan132000@gmail.com>
2022-11-26 23:08:28 +0530

    peer response and status page

--- client/src/components/AddPeerResponse.js
+++ client/src/components/AddPeerResponse.js
@@ -1,13 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
+import "../css/AddPeerResponse.css";
+import { PuffLoader } from "react-spinners";
+import axios from "axios";
 
 function AddPeerResponse(props) {
 	const [cookies, setCookie] = useCookies(["token"]);
+	const [articles, setArticles] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
 	useEffect(() => {
 		if (!cookies.token) props.setDisplayItems(["inline", "inline", "none"]);
 		else props.setDisplayItems(["none", "none", "inline"]);
+
+		const config = {
+			headers: {
+				"Content-Type": "multipart/form-data",
+				"access-control-allow-origin": "*",
+				Authorization: "Bearer " + cookies.token,
+			},
+		};
+		axios.get(`http://localhost:5000/articleStatus`, config).then((res) => {
+			setArticles(res.data);
+			setIsLoading(false);
+			// setJournalIds(
+			// 	res.data.map((journal) => {
+			// 		return journal.journal;
+			// 	})
+			// );
+			console.log(res.data);
+		});
 	}, []);
-	return <div>PeerResponse</div>;
+
+	if (isLoading) {
+		return (
+			<div className="loading-div">
+				<PuffLoader
+					cssOverride={{ display: "inline-block" }}
+					loading={true}
+					size={40}
+					aria-label="Loading Spinner"
+					data-testid="loader"
+				/>{" "}
+				<span className="loading">Loading</span>
+			</div>
+		);
+	} else
+		return (
+			<div className="add-peer-response-div">
+				<div className="review-article-heading">Submit Peer Response</div>
+				{articles.map((article) => {
+					if (article.status === "Under Peer Review")
+						return article.article_name;
+				})}
+			</div>
+		);
 }
 
 export default AddPeerResponse;
--- client/src/components/Status.js
+++ client/src/components/Status.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { PuffLoader } from "react-spinners";
 import { ProgressBar } from "react-milestone";
 import "../css/Status.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Status(props) {
 	const [cookies, setCookie] = useCookies(["token"]);
@@ -23,7 +23,7 @@ function Status(props) {
 	};
 
 	const withdraw = (e) => {
-		const article_id=e.target.value;
+		const article_id = e.target.value;
 		const config = {
 			headers: {
 				"Content-Type": "application/json;charset=UTF-8",
@@ -33,16 +33,11 @@ function Status(props) {
 		};
 
 		axios
-		.post(
-			`http://localhost:5000/withdrawArticle/${article_id}`,
-			{},
-			config
-		)
-		.then((res) => {
-			//Reload
-		});
-	}
-
+			.post(`http://localhost:5000/withdrawArticle/${article_id}`, {}, config)
+			.then((res) => {
+				window.location.reload();
+			});
+	};
 
 	useEffect(() => {
 		props.setDisplayItems(["none", "none", "inline"]);
@@ -56,13 +51,15 @@ function Status(props) {
 		axios.get(`http://localhost:5000/articleStatus`, config).then((res) => {
 			setArticles(res.data);
 			setIsLoading(false);
-			setJournalIds(res.data.map((journal)=>{
-				return journal.journal
-			}));
+			setJournalIds(
+				res.data.map((journal) => {
+					return journal.journal;
+				})
+			);
 		});
 	}, []);
 
-	useEffect(()=>{
+	useEffect(() => {
 		const config = {
 			headers: {
 				"Content-Type": "application/json;charset=UTF-8",
@@ -80,13 +77,14 @@ function Status(props) {
 				config
 			)
 			.then((res) => {
-				setJournalNames(journalIds.map((journalId,index)=>{
-					return res.data[journalId]
-				}))
-
+				setJournalNames(
+					journalIds.map((journalId, index) => {
+						return res.data[journalId];
+					})
+				);
 			});
-	},[journalIds])
-	
+	}, [journalIds]);
+
 	if (isLoading) {
 		return (
 			<div className="loading-div">
@@ -114,43 +112,78 @@ function Status(props) {
 				{articles.map((article, i) => {
 					return (
 						<div key={i} className="article-status-div">
-							<h4 className="article-heading">{article.article_name}
-								<span>
-									<button className="btn article-status-buttons" onClick={withdraw} value={article._id}>Withdraw Article</button>
-									<Link to="/add-peer-response"><button className="btn article-status-buttons">Submit Peer Review</button></Link>
-								</span>
+							<h4 className="article-heading">
+								{article.article_name}
+								{article.status !== "Withdrawn" && (
+									<div className="status-btn-div">
+										<button
+											className="btn article-status-buttons"
+											onClick={withdraw}
+											value={article._id}
+										>
+											Withdraw Article
+										</button>
+										{article.status === "Under Peer Review" && (
+											<Link to="/add-peer-response">
+												<button className="btn article-status-buttons">
+													Submit Peer Review
+												</button>
+											</Link>
+										)}
+									</div>
+								)}
 							</h4>
-							<div className="review-artcile-journal-div">{journalNames[i]}</div>
-							<div className="review-artcile-journal-div">DOS - {new Date(article.date_of_submission).toLocaleDateString('en-GB')}</div>
-							<div className="progress-div">
-								<ProgressBar
-									percentage={progress[article.status]}
-									milestoneCount={5}
-									Milestone={() => (
-										<div className="milestone-circle-incomplete">.</div>
-									)}
-									CurrentMilestone={() => (
-										<div className="milestone-circle-current"></div>
-									)}
-									CompletedMilestone={() => (
-										<div className="milestone-circle">
-											<i class="fas fa-check"></i>
+							<div className="review-artcile-journal-div">
+								{journalNames[i]}
+							</div>
+							<div className="review-artcile-journal-div">
+								DOS -{" "}
+								{new Date(article.date_of_submission).toLocaleDateString(
+									"en-GB"
+								)}
+							</div>
+							{article.status === "Withdrawn" && (
+								<div>
+									<div className="milestone-circle-cross">
+										<i class="fas fa-times-circle"></i>
+									</div>
+									<span className="withdrawn-span">Withdrawn</span>
+								</div>
+							)}
+							{article.status !== "Withdrawn" && (
+								<>
+									<div className="progress-div">
+										<ProgressBar
+											percentage={progress[article.status]}
+											milestoneCount={5}
+											Milestone={() => (
+												<div className="milestone-circle-incomplete">.</div>
+											)}
+											CurrentMilestone={() => (
+												<div className="milestone-circle-current"></div>
+											)}
+											CompletedMilestone={() => (
+												<div className="milestone-circle">
+													<i class="fas fa-check"></i>
+												</div>
+											)}
+											onMilestoneClick={(milestoneIndex) => {}}
+										/>
+									</div>
+									<div className="progress-bar-labels-div">
+										<div className="progress-bar-label">Submitted</div>
+										<div className="progress-bar-label ">Under Review</div>
+										<div className="progress-bar-label">Accepted/Rejected</div>
+										<div className="progress-bar-label center-text">
+											Under Peer Review
 										</div>
-									)}
-									onMilestoneClick={(milestoneIndex) => {}}
-								/>
-							</div>
-							<div className="progress-bar-labels-div">
-								<div className="progress-bar-label">Submitted</div>
-								<div className="progress-bar-label ">Under Review</div>
-								<div className="progress-bar-label">Accepted/Rejected</div>
-								<div className="progress-bar-label center-text">
-									Under Peer Review
-								</div>
-								<div className="progress-bar-label right-text">
-									Peer Accepted/Rejected
-								</div>
-							</div>
+										<div className="progress-bar-label right-text">
+											Peer Accepted/Rejected
+										</div>
+									</div>
+								</>
+							)}
+
 							{/* {article.status} */}
 						</div>
 					);
